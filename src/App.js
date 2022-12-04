c262a5f4ea71112e3e7747f7a399cf9c69c385ad
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-12-04 19:57:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isExpand, setIsExpand] = useState(false);
+
+    const handleChange = () => {
+        setIsExpand(!isExpand);
+    };
+
+    return (
+        <div className="App">
+            <h1 className="bg-success text-white">Conditional Rendering</h1>
+            <button onClick={handleChange}>
+                {!isExpand ? "Xem" : "Đóng"} giới thiệu
+            </button>
+            {isExpand && <p>I love Full-Stack!</p>}
+        </div>
+    );
 }
 
 export default App;
